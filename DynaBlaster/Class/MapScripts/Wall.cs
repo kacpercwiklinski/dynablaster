c8ccc77b5ce2fcbb80f0a734972a0dcf5b3640b1
Kacper <kacper.cwiklinski9@gmail.com>
2019-03-15 15:31:53 +0100

    Top wall textures

--- DynaBlaster/Class/MapScripts/Wall.cs
+++ DynaBlaster/Class/MapScripts/Wall.cs
@@ -7,11 +7,25 @@ using Microsoft.Xna.Framework;
 
 namespace DynaBlaster.Class.MapScripts {
     class Wall : MapObject {
+
+        Vector2 gridPos;
         
-        public Wall(Vector2 pos) : base(pos) {
+        public Wall(Vector2 pos, Vector2 gridPos) : base(pos) {
             this.texture = Game1.textureManager.wall;
             this.walkable = false;
             this.label = "Wall";
+            this.gridPos = gridPos;
+
+            // Setup proper wall texture
+            if(gridPos.X == 0 && gridPos.Y == 0) {
+                this.texture = Game1.textureManager.wall_left_top_corner;
+            }else if (gridPos.X == Map.cols-1 && gridPos.Y == 0) {
+                this.texture = Game1.textureManager.wall_right_top_corner;
+            }else if (gridPos.X % 2 == 0 && gridPos.Y == 0) {
+                this.texture = Game1.textureManager.wall_top_0;
+            } else {
+                this.texture = Game1.textureManager.wall_top_1;
+            }
         }
     }
 }
